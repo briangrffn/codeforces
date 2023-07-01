a31adcc2c69e94570f7aa22d7ae49c0d1aa6832e
Ikromjon <ikromjonsharipov924@gmail.com>
2023-07-01 14:31:29 +0500

    solved P01 Reverse an array

--- Day 1 - Strings and Arrays/P01 Reverse an array/main.cpp
+++ Day 1 - Strings and Arrays/P01 Reverse an array/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    std::cout << "Hfslkdjf" << std::endl;
+    int N;
+    std::cin >> N;
+
+    std::vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> arr[i];
+    }
+
+    // Reverse the array using the reverse function from the algorithm library
+    std::reverse(arr.begin(), arr.end());
+
+    // Print the reversed array
+    for (int i = 0; i < N; i++) {
+        std::cout << arr[i] << " ";
+    }
+
     return 0;
 }
