cbe83f607b14a300997fd98ddf79848ccc22cb7b
Ikromjon <132273377+sharipovikromjon@users.noreply.github.com>
2023-07-09 19:54:25 +0500

    Solved P02 Candies

--- Day 1 - Strings and Arrays/P02 Candies/main.cpp
+++ Day 1 - Strings and Arrays/P02 Candies/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+
+int totalCandies(std::vector<int>& candies, int A, int B) {
+    int sum = 0;
+    for (int i = A; i <= B; i++) {
+        sum += candies[i];
+    }
+    return sum;
+}
 
 int main() {
-    std::cout << "Hi" << std::endl;
+    int N;
+    std::cin >> N;
+
+    std::vector<int> candies(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> candies[i];
+    }
+
+    int A, B;
+    std::cin >> A >> B;
+
+    int total = totalCandies(candies, A, B);
+    std::cout << total << std::endl;
+
     return 0;
 }
+
